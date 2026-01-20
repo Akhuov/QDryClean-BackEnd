24c2a17242bac2ce411d70189e3157be661be43f
ohuno <ohuno@DEFAULT>
2026-01-21 03:31:28 +0500

    implement full Customer CRUD and integrate FluentValidation

--- QDryCleaning.Application/UseCases/charges/Handlers/UpdateChargeCommandHandler.cs
+++ QDryCleaning.Application/UseCases/charges/Handlers/UpdateChargeCommandHandler.cs
@@ -4,7 +4,7 @@ using Microsoft.EntityFrameworkCore;
 using QDryClean.Application.Absreactions;
 using QDryClean.Application.Common.Interfaces.Services;
 using QDryClean.Application.Dtos;
-using QDryClean.Application.Exceptions;
+using QDryClean.Application.Common.Exceptions;
 using QDryClean.Application.UseCases.Charges.Commands;
 
 namespace QDryClean.Application.UseCases.Charges.Handlers
